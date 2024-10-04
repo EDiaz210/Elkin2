74bb53fdbcea08314710d7d67b50e8da673a1e83
EDiaz210 <elkin.diaz@epn.du.ec>
2024-10-04 12:52:20 -0500

    Commit 3

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,10 +1,9 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
-        System.out.printf("2");
-        int edad = 18;
-        if (edad>=18){
-            System.out.println("Usted puede votar en la elecciones 2025");
-        }
+        System.out.println("Elkin Diaz");
+        System.out.println("20");
+        System.out.println("Sangolqui");
+        System.out.println("0967767162");
 
 
     }

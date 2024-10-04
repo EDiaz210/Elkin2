9c19be950fbe66dd5bc5d7d1786b4f7e5afab6f2
EDiaz210 <elkin.diaz@epn.du.ec>
2024-10-04 12:32:03 -0500

    Commit 2

---
+++ src/Ejercicio1.java
@@ -0,0 +1,11 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.printf("2");
+        int edad = 18;
+        if (edad>=18){
+            System.out.println("Usted puede votar en la elecciones 2025");
+        }
+
+
+    }
+}
